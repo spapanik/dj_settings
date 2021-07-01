b61eeacc2b2c03c2b117ce546885fdc0fe33d105
Stephanos Kuma <stephanos@kuma.ai>
2021-07-01 15:12:36 +0300

    Fix failing tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -6,5 +6,5 @@ from dj_settings import utils
 def test_include():
     path = Path(__file__)
     utils.include(path.parent.joinpath("data", "vars.py"))
-    assert lowercase == 1
-    assert UPPERCASE == 2
+    assert lowercase == 1  # noqa: F821
+    assert UPPERCASE == 2  # noqa: F821
