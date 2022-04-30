25b8c8f554c488ca08ce5ce2f1284eab477280be
Stephanos Kuma <stephanos@kuma.ai>
2022-04-30 19:05:42 +0300

    Add tests

--- tests/dj_settings/test_utils.py
+++ tests/dj_settings/test_utils.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 
 import pytest
 
@@ -9,3 +10,13 @@ from dj_settings import utils
 def test_setting(allow_env, expected):
     os.environ["VAR"] = "env"
     assert utils.setting("VAR", allow_env=allow_env, default="default") == expected
+
+
+class TestSettingsParser:
+    @staticmethod
+    @pytest.mark.parametrize(["suffix"], [[".ini"], [".json"], [".toml"], [".yaml"]])
+    def test_data(suffix):
+        file = Path(__file__).parents[1].joinpath("data/settings").with_suffix(suffix)
+        assert utils.SettingsParser(file).data == {
+            "database": {"username": "aria.stark", "password": "valar morghulis"}
+        }
