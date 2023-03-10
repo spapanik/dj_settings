88569fd916b2973971692bae60e236ad40cbca5d
Stephanos Kuma <stephanos@kuma.ai>
2023-03-10 22:05:55 +0000

    Add some more rules

--- tests/dj_settings/test_utils.py
+++ tests/dj_settings/test_utils.py
@@ -6,7 +6,7 @@ import pytest
 from dj_settings import utils
 
 
-@pytest.mark.parametrize(["allow_env", "expected"], [[True, "env"], [False, "default"]])
+@pytest.mark.parametrize(("allow_env", "expected"), [(True, "env"), (False, "default")])
 def test_setting(allow_env, expected):
     os.environ["VAR"] = "env"
     assert utils.setting("VAR", allow_env=allow_env, default="default") == expected
@@ -14,7 +14,7 @@ def test_setting(allow_env, expected):
 
 class TestSettingsParser:
     @staticmethod
-    @pytest.mark.parametrize(["suffix"], [[".ini"], [".json"], [".toml"], [".yaml"]])
+    @pytest.mark.parametrize("suffix", [".ini", ".json", ".toml", ".yaml"])
     def test_data(suffix):
         file = Path(__file__).parents[1].joinpath("data/settings").with_suffix(suffix)
         assert utils.SettingsParser(file).data == {
@@ -22,7 +22,7 @@ class TestSettingsParser:
         }
 
     @staticmethod
-    @pytest.mark.parametrize(["suffix"], [[".ini"], [".json"], [".toml"], [".yaml"]])
+    @pytest.mark.parametrize("suffix", [".ini", ".json", ".toml", ".yaml"])
     def test_data_order(suffix):
         file = Path(__file__).parents[1].joinpath("data/settings").with_suffix(suffix)
         database = utils.SettingsParser(file).data["database"]
@@ -41,14 +41,14 @@ class TestSettingsParser:
 
 
 @pytest.mark.parametrize(
-    ["base", "override", "expected"],
+    ("base", "override", "expected"),
     [
-        [{"list": [1, 2, 3]}, {"list": [4, 5]}, {"list": [4, 5]}],
-        [
+        ({"list": [1, 2, 3]}, {"list": [4, 5]}, {"list": [4, 5]}),
+        (
             {"dict": {"x": 1, "y": 2}},
             {"dict": {"x": "a", "z": "b"}},
             {"dict": {"x": "a", "y": 2, "z": "b"}},
-        ],
+        ),
     ],
 )
 def test_deep_merge(base, override, expected):
