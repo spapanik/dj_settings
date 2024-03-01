f4c79cfd1c3cc1df8fe5fbe8496ab10d97ee08bf
Stephanos Kuma <stephanos@kuma.ai>
2024-03-01 15:53:46 +0000

    Allow using the XDG env var for home config

--- src/dj_settings/constants.py
+++ src/dj_settings/constants.py
@@ -1,10 +1,11 @@
 from __future__ import annotations
 
+import os
 from pathlib import Path
 from typing import get_args
 
 from dj_settings.types import SupportedType
 
 ETC = Path("/etc/")
-HOME_CONF = Path.home().joinpath(".config/")
+HOME_CONF = Path(os.environ.get("XDG_CONFIG_HOME", Path.home().joinpath(".config/")))
 SUPPORTED_TYPES = frozenset(get_args(SupportedType))
