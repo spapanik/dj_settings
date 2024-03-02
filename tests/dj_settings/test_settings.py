665358452fc6439b0e21e0513edf286adf474290
Stephanos Kuma <stephanos@kuma.ai>
2024-03-02 19:39:46 +0000

    Add more tests

--- tests/dj_settings/test_settings.py
+++ tests/dj_settings/test_settings.py
@@ -8,8 +8,34 @@ import pytest
 from dj_settings import settings
 
 
+@pytest.mark.parametrize(
+    ("attribute", "allow_env", "expected"),
+    [
+        ("USER", True, "Monsieur Madeleine"),
+        ("USER", False, "Jean Valjean"),
+        ("email", True, "madeleine@montreuil.gov"),
+        ("email", False, "madeleine@montreuil.gov"),
+    ],
+)
+def test_setting(
+    data_dir: Path, attribute: str, allow_env: bool, expected: str
+) -> None:
+    os.environ["USER"] = "Monsieur Madeleine"
+    assert (
+        settings.setting(
+            attribute,
+            allow_env=allow_env,
+            base_dir=data_dir,
+            filename="config.yml",
+            sections=["info"],
+            default="default",
+        )
+        == expected
+    )
+
+
 @pytest.mark.parametrize(("allow_env", "expected"), [(True, "env"), (False, "default")])
-def test_setting(allow_env: bool, expected: str) -> None:
+def test_setting_without_file(allow_env: bool, expected: str) -> None:
     os.environ["VAR"] = "env"
     assert settings.setting("VAR", allow_env=allow_env, default="default") == expected
 
@@ -17,22 +43,22 @@ def test_setting(allow_env: bool, expected: str) -> None:
 class TestSettingsParser:
     @staticmethod
     @pytest.mark.parametrize("suffix", [".ini", ".json", ".toml", ".yaml"])
-    def test_data(suffix: str) -> None:
-        file = Path(__file__).parents[1].joinpath("data/settings").with_suffix(suffix)
+    def test_data(data_dir: Path, suffix: str) -> None:
+        file = data_dir.joinpath("settings").with_suffix(suffix)
         assert settings.SettingsParser(file).data == {
             "database": {"username": "aria.stark", "password": "valar morghulis"}
         }
 
     @staticmethod
     @pytest.mark.parametrize("suffix", [".ini", ".json", ".toml", ".yaml"])
-    def test_data_order(suffix: str) -> None:
-        file = Path(__file__).parents[1].joinpath("data/settings").with_suffix(suffix)
+    def test_data_order(data_dir: Path, suffix: str) -> None:
+        file = data_dir.joinpath("settings").with_suffix(suffix)
         database = settings.SettingsParser(file).data["database"]
         assert list(database) == ["username", "password"]
 
     @staticmethod
-    def test_overriding() -> None:
-        file = Path(__file__).parents[1].joinpath("data/override.toml")
+    def test_overriding(data_dir: Path) -> None:
+        file = data_dir.joinpath("override.toml")
         assert settings.SettingsParser(file).data == {
             "foo": {"x": 100, "y": 20, "z": 3}
         }
--- tests/dj_settings/test_utils.py
+++ tests/dj_settings/test_utils.py
@@ -1,8 +1,39 @@
 from __future__ import annotations
 
+from pathlib import Path
+
 import pytest
 
 from dj_settings import utils
+from dj_settings.types import SupportedType
+
+
+@pytest.mark.parametrize(
+    ("filename", "same_suffix", "expected_paths"),
+    [
+        (".env", False, [Path(".env.d/.env.01")]),
+        (
+            "override.toml",
+            True,
+            [
+                Path("override.toml"),
+                Path("override.toml.d/01-this.toml"),
+                Path("override.toml.d/02-that.toml"),
+            ],
+        ),
+    ],
+)
+def test_get_override_paths(
+    data_dir: Path, filename: str, same_suffix: bool, expected_paths: list[Path]
+) -> None:
+    env_path = data_dir.joinpath(filename)
+    expected_paths = [
+        data_dir.joinpath(expected_path) for expected_path in expected_paths
+    ]
+    assert (
+        list(utils.get_override_paths(env_path, same_suffix=same_suffix))
+        == expected_paths
+    )
 
 
 @pytest.mark.parametrize(
@@ -22,3 +53,25 @@ def test_deep_merge(
     expected: dict[str, list[int]],
 ) -> None:
     assert utils.deep_merge(base, override) == expected
+
+
+@pytest.mark.parametrize(
+    ("path", "force_type", "expected"),
+    [
+        (Path("settings.ini"), None, "ini"),
+        (Path("settings.ini"), "toml", "toml"),
+        (Path(".env"), None, "env"),
+        (Path(".env"), "json", "json"),
+    ],
+)
+def test_get_type(
+    path: Path, force_type: SupportedType | None, expected: SupportedType
+) -> None:
+    assert utils.get_type(path, force_type) == expected
+
+
+def test_extract_data(data_dir: Path) -> None:
+    path = data_dir.joinpath("settings.toml")
+    assert utils.extract_data(path, "toml") == {
+        "database": {"username": "aria.stark", "password": "valar morghulis"}
+    }
