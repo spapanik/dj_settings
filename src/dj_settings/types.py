5b2b74415406ea611f5594edf067a39f0a0db7a8
Stephanos Kuma <stephanos@kuma.ai>
2024-12-11 12:00:27 +0000

    🧹 Remove unnecessary type

--- src/dj_settings/types.py
+++ src/dj_settings/types.py
@@ -1,8 +1,6 @@
 from __future__ import annotations
 
-from pathlib import Path
-from typing import Any, Literal, Union
+from typing import Any, Literal
 
 ConfDict = dict[str, Any]
-PathConf = Union[str, Path, ConfDict]
 SupportedType = Literal["json", "env", "yaml", "ini", "toml"]
